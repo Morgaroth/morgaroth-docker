4c7d657c61e1721ce33feebcfddd29559a236bd0
MorgarothXPS <morgaroth@morgaroth.xps>
2016-10-11 10:56:28 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='morgaroth-docker',
+      version='0.0.1',
+      description='Morgaroths Docker Client',
+      url='http://github.com/storborg/funniest',
+      author='Mateusz Jaje',
+      author_email='mateuszjaje@gmail.com',
+      license='MIT',
+      packages=['morgaroth_docker'],
+      zip_safe=False)

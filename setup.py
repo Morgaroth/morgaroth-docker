e41a5d32d0db145674d83a35f9626685bed8702e
Morgaroth <mateuszjaje@gmail.com>
2016-10-11 11:10:58 +0200

    first version of morgaroths docker registry client

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='morgaroth-docker',
       version='0.0.1',
       description='Morgaroths Docker Client',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/Morgaroth/morgaroth-docker',
       author='Mateusz Jaje',
       author_email='mateuszjaje@gmail.com',
       license='MIT',
       packages=['morgaroth_docker'],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3 :: Only',
+      ],
+      )
